2d378951854da5ecb19e07a136ca596c8b1e051a
lwtaylor <lwtaylor@ucsd.edu>
2024-05-01 18:59:28 -0700

    added bar plot

--- notebooks/app.py
+++ notebooks/app.py
@@ -26,56 +26,66 @@ def main():
         #label = classify_audio(audio_file)
         #st.write(f"The predicted category is: {label}")
         st.write("Audio file uploaded")
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        st.write(f"Using {device}")
+        signal, sr = torchaudio.load(audio_file)
+        signal = signal.to(device)
+
+        hyperparameters = dict(
+        target_sample_rate=16000,
+        num_samples=22050, 
+        n_fft=1024, 
+        hop_length=512, 
+        n_mels=64, 
+        sr = sr
+        )
+
+        # process the signal so it's in desired format
+        signal = processing_pipeline(signal, hyperparameters)
+
+        mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+            sample_rate=hyperparameters['target_sample_rate'],
+            n_fft=hyperparameters['n_fft'],
+            hop_length=hyperparameters['hop_length'],
+            n_mels=hyperparameters['n_mels']
+        )
+
+        # apply transformation to the signal
+        signal = mel_spectrogram(signal)
+
+        # save the mel_spectrogram so that we can show the
+        # user what it looks like
+        spectrogram_output = signal 
+
+        # convert to decibels (kinda)
+        signal = torch.log(signal + 0.00001)
+
+        # add a channel dimension so that model thinks it's a batch
+        signal = signal.unsqueeze(0) 
+
+        # Load the model
+        model = CNNNetwork().to(device)
+        model.load_state_dict(torch.load('feedforwardnet.pth', map_location=torch.device("cpu")))
+        model.eval()
+        # Make a prediction
+
+        emotion_map = ['HAP', 'NEU', 'ANG', 'FEA', 'DIS', 'SAD']
+
+        with torch.no_grad():
+            predictions = model(signal)
+            st.write('Predictions:', predictions)
+            predicted_index = predictions.argmax(1).item()
+            st.write(f'Predicted class index: {emotion_map[predicted_index]}')
+
+            predictions = predictions.squeeze().numpy()
+            st.write(predictions)
+
+            df = pd.DataFrame({'emotion': emotion_map, 'probability': predictions})
+            st.bar_chart(data=df, x='emotion', y='probability', use_container_width=True)
+
+
     
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    st.write(f"Using {device}")
-    signal, sr = torchaudio.load(audio_file)
-    signal = signal.to(device)
-
-    hyperparameters = dict(
-    target_sample_rate=16000,
-    num_samples=22050, 
-    n_fft=1024, 
-    hop_length=512, 
-    n_mels=64, 
-    sr = sr
-    )
-
-    # process the signal so it's in desired format
-    signal = processing_pipeline(signal, hyperparameters)
-
-    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-        sample_rate=hyperparameters['target_sample_rate'],
-        n_fft=hyperparameters['n_fft'],
-        hop_length=hyperparameters['hop_length'],
-        n_mels=hyperparameters['n_mels']
-    )
-
-    # apply transformation to the signal
-    signal = mel_spectrogram(signal)
-
-    # save the mel_spectrogram so that we can show the
-    # user what it looks like
-    spectrogram_output = signal 
-
-    # convert to decibels (kinda)
-    signal = torch.log(signal + 0.00001)
-
-    # add a channel dimension so that model thinks it's a batch
-    signal = signal.unsqueeze(0) 
-
-    # Load the model
-    model = CNNNetwork().to(device)
-    model.load_state_dict(torch.load('feedforwardnet.pth', map_location=torch.device("cpu")))
-    model.eval()
-    # Make a prediction
-
-    with torch.no_grad():
-        predictions = model(signal)
-        st.write('Predictions:', predictions)
-        predicted_index = predictions.argmax(1).item()
-        st.write(f'Predicted class index: {predicted_index}')
-
 
 if __name__ == "__main__":
     main()
