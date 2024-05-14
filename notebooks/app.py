1bd1675e98a7efe630b1da7dbdc319dbfce58071
lwtaylor <lwtaylor@ucsd.edu>
2024-05-13 17:31:08 -0700

    app and images

--- notebooks/app.py
+++ notebooks/app.py
@@ -1,12 +1,15 @@
-import streamlit as st
-import torch
-from io import BytesIO
-#from your_pytorch_model_module import classify_audio  # Ensure this is your PyTorch model function
+import streamlit as st 
+import torch 
+
+# python package to manage binary data and treat it as a file
+# without even needing to write it to disk
+from io import BytesIO 
 #from AudioModel import * # CNNNetwork()
 from AudioDataset import * # AudioEmotionDataset()
 
-from cnnnet import * # impport model
+from cnnnet import * # import Vrisan's model 
 
+# Pytorch Ploting Functions required imports 
 from IPython.display import Audio
 from matplotlib.patches import Rectangle
 from torchaudio.utils import download_asset
@@ -16,7 +19,6 @@ from torchaudio.utils import download_asset
 from audio_recorder_streamlit import audio_recorder
 
 
-# TODO: return the figure from the following funtions for streamlit plotting purposes
 # https://pytorch.org/audio/main/tutorials/audio_feature_extractions_tutorial.html#sphx-glr-tutorials-audio-feature-extractions-tutorial-py
 def plot_waveform(waveform, sr, title="Waveform", ax=None):
     waveform = waveform.numpy()
@@ -32,59 +34,62 @@ def plot_waveform(waveform, sr, title="Waveform", ax=None):
     ax.set_title(title)
 
     st.pyplot(fig)
-
-
-def plot_spectrogram(specgram, title=None, ylabel="freq_bin", ax=None):
-    if ax is None:
-        fig, ax = plt.subplots(1, 1)
+    
+def plot_spectrogram(specgram, title=None, ylabel="freq_bin (decibels)"):
+    # Creating a figure and an axis object
+    fig, ax = plt.subplots(1, 1)
+    
     if title is not None:
         ax.set_title(title)
     ax.set_ylabel(ylabel)
-    ax.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto", interpolation="nearest")
 
+    # Convert power spectrogram to decibel units
+    db_specgram = librosa.power_to_db(specgram) # np.log(specgram + 1) 
+
+    # Plot the masked spectrogram
+    im = ax.imshow(db_specgram, origin="lower", aspect="auto", interpolation="nearest", cmap='inferno')
+    fig.colorbar(im, ax=ax)  # Adding a colorbar
+
+    # Display the plot in Streamlit
     st.pyplot(fig)
 
-
+# TODO: research what a filter bank is? do we need this? 
 def plot_fbank(fbank, title=None):
     fig, axs = plt.subplots(1, 1)
     axs.set_title(title or "Filter bank")
     axs.imshow(fbank, aspect="auto")
     axs.set_ylabel("frequency bin")
-    axs.set_xlabel("mel bin")
+    axs.set_xlabel("mel bin") # only for mel spectrograms? 
 
     st.pyplot(fig)
 
-
+# TODO: add this to the Readme.md file to run our app 
 # streamlit run app.py --server.enableXsrfProtection false
 # this prevents the error: AxiosError: Request failed with status code 403
 def main():
     st.title('Audio Emotion Classifier')
     
-    # Upload audio file or record audio
+    # option for user to upload a file from their computer
     audio_file = st.file_uploader("Upload an audio file", type=['wav'])
-    # record = st.checkbox("Or record audio")
 
-    # if record:
-    #     audio_data = st.audio_recorder("Record your audio", type="wav", time_limit=10)
-    #     if audio_data:
-    #         audio_bytes = audio_data.read()
-    #         audio_file = BytesIO(audio_bytes)
+    # option for user to record audio
     audio_bytes = audio_recorder()
+
     if audio_bytes:
-        # playback audio
+        # creates a bytesio object that behaves like a file, but is stored within memory
         audio_file = BytesIO(audio_bytes)
 
     if audio_file is not None:
-        # Assuming classify_audio expects a file-like object
-        #label = classify_audio(audio_file)
-        #st.write(f"The predicted category is: {label}")
 
-        st.audio(audio_bytes, format="audio/wav")
+        # displays an audio player so that the user can listen to the audio file
+        st.audio(audio_file, format="audio/wav")
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # load file as pytorch tensor
         signal, sr = torchaudio.load(audio_file)
         signal = signal.to(device)
 
+        # hyperparameters for the model and processing pipeline
         hyperparameters = dict(
         target_sample_rate=16000,
         num_samples=60000, # 22050
@@ -96,12 +101,15 @@ def main():
 
 
         # process the signal so it's in desired format
+
+        # imported from audio_processing_functions.py
         signal = processing_pipeline(signal, hyperparameters)
 
         waveform_signal = signal # Save the waveform signal for later
         print(signal.shape)
 
         plot_waveform(signal, sr, title="Waveform")
+
 
         mel_spectrogram = torchaudio.transforms.MelSpectrogram(
             sample_rate=hyperparameters['target_sample_rate'],
@@ -120,9 +128,11 @@ def main():
         spectrogram_output = signal.squeeze() 
 
         print(spectrogram_output.shape)
+        # TODO: try plotting with librosa instead
         plot_spectrogram(spectrogram_output, title="Mel Spectrogram")
 
         # convert to decibels (kinda)
+        # is there a way we can improve on this?
         signal = torch.log(signal + 0.00001)
 
         # add a channel dimension so that model thinks it's a batch
@@ -138,7 +148,7 @@ def main():
 
         with torch.no_grad():
             predictions = model(signal)
-            st.write('Predictions:', predictions)
+            #st.write('Predictions:', predictions)
             predicted_index = predictions.argmax(1).item()
             st.write(f'Predicted class index: {emotion_map[predicted_index]}')
 
